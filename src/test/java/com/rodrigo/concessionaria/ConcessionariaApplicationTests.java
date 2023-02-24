18a041152b53b07786f23c3e7e2775848bb45cd6
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-24 09:01:45 -0300

    projeto  concessionaria

---
+++ src/test/java/com/rodrigo/concessionaria/ConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.concessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
